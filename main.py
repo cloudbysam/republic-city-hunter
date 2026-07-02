a87124eabecf982f79a69389abd351b00229a978
Samuel Ajala <samuelaj220@gmail.com>
2026-07-02 01:57:05 +0100

    initialize the game.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 print("Welcome to the streets of Republic City, Equalist Vigilante..")
 
 player = {
-    'name' : input("\nenter player name: ").capitalize(),
+    'name' : input("enter player name: ").capitalize(),
     'health' : 30,
     'yuans' : 10,
-    'inventory' : ['laser pistol']
+    'inventory' : ['electric shock gloves']
  }
 
 def show_status():
@@ -17,7 +17,7 @@ def show_status():
 
 def visit_underground_hideout():
     print("\n Slip into the Republic City underground tunnels... undetected.")
-    remains = player["credits"]
+    remains = player["yuans"]
 
     if remains < 15:
         raise ValueError("Not enough yuans!")
@@ -75,3 +75,35 @@ def visit_amon_portal():
 
     except ValueError as err:
         print(f"Insufficient funds!!! {err}!!")
+
+while True:
+    show_status() # not yet defined
+    print(f"\n what do you want to do? {player["name"]}")
+    print("1. visit_underground_hideout (Heal). ")
+    print("2. Hunt down benders (Earn yuans). ")
+    print("3. visit_amon_portal (illegal shop). ")
+    print("4. If you are scared to challenge the Avatar....QUIT.")
+
+    try:
+        choice = int(input("\n Enter Choice (1-4): "))
+
+        if choice == 1:
+            visit_underground_hideout()
+
+        elif choice == 2:
+            hunt_down_benders()
+
+        elif choice == 3:
+            visit_amon_portal()
+
+        elif choice == 4:
+            print("\n You fled the city on a Sato-Corp airship. The Avatar wins this round...")
+            break
+        else:
+            print("Invalid sector code: Pick 1,2,3 or 4.")
+
+    except ValueError as error:
+        if "invalid literal" in str(error):
+            print("system error: You must enter a digit (number) !!!")
+        else:
+            print(f"system error: {error}")
