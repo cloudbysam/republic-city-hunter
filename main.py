c5ae590160ad0c5ed4f2b0ae442648f91ad45222
Samuel Ajala <samuelaj220@gmail.com>
2026-07-02 01:27:25 +0100

    add hunt down benders func.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Welcome to the streets of Republic City, Equalist Vigilante..")
 player = {
     'name' : input("\nenter player name: ").capitalize(),
     'health' : 30,
-    'credits' : 10,
+    'yuans' : 10,
     'inventory' : ['laser pistol']
  }
 
@@ -28,3 +28,12 @@ def visit_underground_hideout():
         player["health"] = 100
 
     print(" Wounds patched up by Equalist medics! Cost: 15 Yuans.")
+
+def hunt_down_benders():
+    print("\n ^***^ Hunting down benders :) ^***^")
+    if player["health"] < 20:
+        raise ValueError("Too weak to hunt!! Visit the temple first..")
+    player["health"] -= 20
+    player["credits"] += 40
+
+    print("Shock gloves activated! You stripped a rogue bender of their elements. Amon is pleased.")
